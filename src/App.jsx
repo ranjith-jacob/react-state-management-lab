a231ecbb2b215dcd8824f9ecb38de67628515f09
ranjith.j <ranjithjacob.work@gmail.com>
2026-01-28 13:48:49 +0000

    midway

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,132 @@
+import('./App.css'); 
+import { useState } from 'react';
+
 const App = () => {
 
-  return (
-    <h1>Hello world!</h1>
-  );
-}
+const [team, setTeam] = useState([]);
+const [money, setMoney] = useState(100); 
 
-export default App
+const zombieFighters = [
+  {
+    id: 1,
+    name: 'Survivor',
+    price: 12,
+    strength: 6,
+    agility: 4,
+    img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/0c2d6b.png',
+  },
+  {
+    id: 2,
+    name: 'Scavenger',
+    price: 10,
+    strength: 5,
+    agility: 5,
+    img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/033a16.png',
+  },
+  {
+    id: 3,
+    name: 'Shadow',
+    price: 18,
+    strength: 7,
+    agility: 8,
+    img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/262c36.png',
+  },
+  {
+    id: 4,
+    name: 'Tracker',
+    price: 14,
+    strength: 7,
+    agility: 6,
+    img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/3c1e70.png',
+  },
+  {
+    id: 5,
+    name: 'Sharpshooter',
+    price: 20,
+    strength: 6,
+    agility: 8,
+    img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/4b2900.png',
+  },
+  {
+    id: 6,
+    name: 'Medic',
+    price: 15,
+    strength: 5,
+    agility: 7,
+    img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/5a1e02.png',
+  },
+  {
+    id: 7,
+    name: 'Engineer',
+    price: 16,
+    strength: 6,
+    agility: 5,
+    img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/5e103e.png',
+  },
+  {
+    id: 8,
+    name: 'Brawler',
+    price: 11,
+    strength: 8,
+    agility: 3,
+    img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/67060c.png',
+  },
+  {
+    id: 9,
+    name: 'Infiltrator',
+    price: 17,
+    strength: 5,
+    agility: 9,
+    img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/ac3220.png',
+  },
+  {
+    id: 10,
+    name: 'Leader',
+    price: 22,
+    strength: 7,
+    agility: 6,
+    img: 'https://pages.git.generalassemb.ly/modular-curriculum-all-courses/react-state-management-lab/assets/e41f26.png',
+  },
+]
+
+
+const handleAddFighter = (newTeam) => {
+  const zombieTeam = [...team, newTeam] 
+  setTeam(zombieTeam); 
+}; 
+
+zombieTeam.map((zombie) => {
+    const newMoney = () => {
+      setMoney(prevMoney => prevMoney - zombie.price); 
+      newMoney(); 
+    }; 
+});
+
+
+  return (
+    <>
+    <h1>Hello world!</h1>
+    <h2>Money: £{money} </h2>
+    <h2>Team: {team.map((zombie, index) => (
+      <p key={index}>{zombie.name}</p>
+    ))} 
+      </h2>
+    <ul>
+      {zombieFighters.map((zombie) => (
+        <li key={zombie.id}>
+        <p>{zombie.name}</p>
+        <p>{zombie.price}</p>
+        <p>{zombie.strength}</p>
+        <p>{zombie.agility}</p>
+        <img src={zombie.img} />
+        <button onClick={() => handleAddFighter(zombie)}>Add Zombie</button>
+        </li>
+      ))}
+    </ul>
+    </>
+  );
+}; 
+
+
+
+export default App; 
